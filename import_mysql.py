67c7f6c43534cd2eddafd5c98166e8baeead5633
NIHAL N <nihaal21075@gmail.com>
2026-05-24 11:42:19 +0530

    Update import_mysql.py

--- import_mysql.py
+++ import_mysql.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # read csv
-df = pd.read_csv("cleaned_train.csv") 
+df = pd.read_csv("cleaned_train.csv")  
 
 # take only 1000 rows
 df = df.head(1000)
